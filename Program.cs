7966e4f2ab9e74fdbb6083b17803481bc939ed12
Danielle Iannuzzi <danielle.iannuzzi@hotmail.com>
2019-07-22 09:16:58 -0300

    Realizando cálculos

--- Program.cs
+++ Program.cs
@@ -9,12 +9,18 @@ namespace Tabuada
            int numero;
            //limpar a tela de console
            Console.Clear();
-           Console.WriteLine("###################");
-           Console.WriteLine("#                                    Programa de tabuada                   #");
-           Console.WriteLine("####################");
+           Console.WriteLine("###################################################################");
+           Console.WriteLine("#                           Programa de tabuada                   #");
+           Console.WriteLine("###################################################################");
            Console.WriteLine();
-           Console.WriteLine("Digite um número > ");
-           
+           Console.WriteLine("Digite um número : ");
+           numero = int.Parse(Console.ReadLine());
+
+           for(int x = 0; x <= 10; x++){
+               Console.WriteLine(numero+" x "+x+" = " + numero*x );
+           }
+
+
         }
     }
 }
