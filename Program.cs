83bcec56b6d36dc244ac5aac8801bf8303cd7355
Danielle Iannuzzi <danielle.iannuzzi@hotmail.com>
2019-07-22 08:54:27 -0300

    Estrutura

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int numero;
+           //limpar a tela de console
+           Console.Clear();
+           Console.WriteLine("###################");
+           Console.WriteLine("#                                    Programa de tabuada                   #");
+           Console.WriteLine("####################");
+           Console.WriteLine();
+           Console.WriteLine("Digite um número > ");
+           
         }
     }
 }
